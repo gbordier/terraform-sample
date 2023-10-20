b762b3645cf48e2f7d4a75260a185aadf4ab302f
Guillaume Bordier <gbordier@microsoft.com>
2023-10-21 00:31:29 +0200

    P

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,17 @@
-folder=${PWD##*/}
+. ./.env.sh
 
-. ../pipelines/$folder/.env.sh
+[[ -z $TENANT_ID  || -z $SUBSCRIPTION_ID || -z $PREFIX || -z $ENV || -z $FOLDER ]] && echo "missing env vars look for instructions in env.sh.template" && exit 1
 
-[[ -z $TENANT_ID  || -z $SUBSCRIPTION_ID || -z $PREFIX || -z $ENV ]] && echo "missing env vars look for instructions in env.sh.template" && exit 1
+
+cd ./infra/$FOLDER
 
 ## load variables and secrets from keyvault
 keyVaultName=$PREFIX-$ENV-pl-kv
-tenant_id=$(cat ./tf-vars/${ENV}.json  | jq -r '."tenant-id"' | sed 's/"//g')
+tenant_id=$TENANT_ID
+
+## TODO we can use OIDC also in builtin mode 
+
+
 
 ## this will provoke an interactive login
 [[ $(az account get-access-token -o tsv --query "expiresOn")  < $(date +"%Y-%m-%d %H:%M:%S") ]] &&  az login --tenant "$tenant_id"
@@ -15,8 +20,6 @@ az keyvault secret list --vault-name $keyVaultName
 ## cauthion but loads secrets as environment variables replace dashes with underscores
 eval $(az keyvault secret list --vault-name $keyVaultName --query "[].[name]" -o tsv | xargs -l1 az keyvault secret show --vault-name $keyVaultName   --name |  jq -r '(.name | gsub("-";"_"))  +"="+ (.value | @sh) '  )
 
-## cd to the terrform folder for this part
-cd ../../$folder
 
 ## shell variable do not support dashes :( 
 terraform init -backend-config="storage_account_name=${PREFIX}${ENV}tfsa" \
