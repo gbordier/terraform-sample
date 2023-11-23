b063a48484824ae40a24b770a65b1a4dbcd09ab7
Guillaume Bordier <gbordier@microsoft.com>
2023-11-23 16:15:01 +0100

    p

--- infra/adx/09-eventgrid.tf
+++ infra/adx/09-eventgrid.tf
@@ -17,7 +17,7 @@ resource "azurerm_eventgrid_system_topic" "nsglogs" {
 resource "azurerm_eventgrid_system_topic_event_subscription" "nsgflowlog-west" {
   name                = "eventgrid-nsgflowlog-west"
   system_topic        = azurerm_eventgrid_system_topic.nsglogs.name
-  resource_group_name = data.azurerm_resource_group.spoke
+  resource_group_name = data.azurerm_resource_group.spoke.name
   eventhub_endpoint_id = azurerm_eventhub.firewall.id
   included_event_types = [
     "Microsoft.Storage.BlobCreated"
