6a94f9ad55420dbc51340eef3976830e937d5e8d
Guillaume Bordier <gbordier@microsoft.com>
2023-11-23 12:42:49 +0100

    p

--- infra/adx/03-data.tf
+++ infra/adx/03-data.tf
@@ -2,21 +2,18 @@ data "azurerm_client_config" "current" {
 }
 
 
-
-/*
 data "azurerm_resource_group" "main-rg" {
-  provider = azurerm
+  provider = azurerm.connectivity
   name = "${var.prefix}-${var.env}-main-rg"
   // name     = format("%s-%s-%s-%s-hub-rg", var.site_code_hub, var.client_code_hub, var.aip_code_hub, var.environment)
 }
 
 
 data "azurerm_resource_group" "spoke" {
-  provider = azurerm
+  provider = azurerm.connectivity
   name = "${var.prefix}-${var.env}-spoke-rg"
   // name     = format("%s-%s-%s-%s-hub-rg", var.site_code_hub, var.client_code_hub, var.aip_code_hub, var.environment)
 }
-
 
 /* references hub vnet  for routes
 data "azurerm_virtual_network" "hub" {
